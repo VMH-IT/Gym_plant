30d000f767d00b12c6ea7ca398a6db7d5a6c277d
VMH-IT <vmh.it170701@gmail.com>
2022-09-28 15:03:47 +0700

    Chang 1 file to check git

--- server/app/models/gymer.rb
+++ server/app/models/gymer.rb
@@ -4,4 +4,5 @@ class Gymer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
 end
